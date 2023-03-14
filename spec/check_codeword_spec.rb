943a9af6c3611e6aebb8bb927e60371b9e3a54ba
Madelane <madelanedaz@gmail.com>
2023-03-14 12:07:24 +0000

    create tests for exercise 2

---
+++ spec/check_codeword_spec.rb
@@ -0,0 +1,19 @@
+require 'check_codeword'
+
+RSpec.describe "add check_codeword method" do
+  it "returns a correct message if given the right codeword" do
+    result = check_codeword("horse")
+    expect(result).to eq "Correct! Come in."
+  end
+  
+  it "returns a wrong message if given the wrong codeword" do
+    result = check_codeword("lamp")
+    expect(result).to eq "WRONG!"
+  end
+
+  it "returns a close messsage if given a codeword that has similar first and last character" do
+    result = check_codeword("home")
+    expect(result).to eq "Close, but nope."
+  end
+
+end
