b7d8bec8c8c138e4ca8b0c1bf88164f2b04a8564
Madelane <madelanedaz@gmail.com>
2023-03-14 19:46:56 +0000

    create tests for exercise 2, testing classes

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Counter do
     result = counter.report
     expect(result).to eq "Counted to 0 so far."
   end
+
+  it "reports a count of added value with one addition" do
+    counter = Counter.new
+    counter.add(10)
+    expect(counter.report).to eq "Counted to 10 so far."
+  end
+
+  it "reports a count of all added values" do
+    counter = Counter.new
+    counter.add(10)
+    counter.add(5)
+    counter.add(6)
+    expect(counter.report).to eq "Counted to 21 so far."
+  end
+  
 end
---
+++ spec/string_builder_spec.rb
@@ -0,0 +1,45 @@
+require 'string_builder'
+
+RSpec.describe StringBuilder do
+  it "initially returns an empty string as output" do
+    string = StringBuilder.new
+    expect(string.output).to eq ""
+  end
+
+  it "initially returns a string's size of zero" do
+    string = StringBuilder.new
+    expect(string.size).to eq 0
+  end
+
+  context "given one addition of a string" do
+    it "outputs the string" do
+      string = StringBuilder.new
+      string.add("Hi")
+      expect(string.output).to eq "Hi"
+    end
+
+    it "returns the string's size" do
+      string = StringBuilder.new
+      string.add("Hi")
+      expect(string.size).to eq 2
+    end
+  end
+
+  context "given multiple additions of a string" do
+    it "outputs the string" do
+      string = StringBuilder.new
+      string.add("Hi")
+      string.add("lovely")
+      string.add("human")
+      expect(string.output).to eq "Hilovelyhuman"
+    end
+
+    it "returns the string's size" do
+      string = StringBuilder.new
+      string.add("Hi")
+      string.add("lovely")
+      string.add("human")
+      expect(string.size).to eq 13
+    end
+  end
+end
