17be32053624452107f8bb95ddcdbfd431dba2f5
Madelane <madelanedaz@gmail.com>
2023-03-14 18:43:11 +0000

    create test for exercise 1, testing classes wih equality

---
+++ spec/counter_spec.rb
@@ -0,0 +1,9 @@
+require 'counter'
+
+RSpec.describe Counter do
+  it "initially report a count of zero" do
+    counter = Counter.new
+    result = counter.report
+    expect(result).to eq "Counted to 0 so far."
+  end
+end
