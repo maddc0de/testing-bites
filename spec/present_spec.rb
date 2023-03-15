216e3ad31047539a2284498c47394e6ff0939fe4
Madelane <madelanedaz@gmail.com>
2023-03-15 11:53:23 +0000

    create tests for exercise one, testing for errors

---
+++ spec/present_spec.rb
@@ -0,0 +1,28 @@
+require 'present'
+
+RSpec.describe Present do
+  it "wraps and unwrap a value" do
+    present = Present.new
+    present.wrap("mug")
+    expect(present.unwrap). to eq "mug"
+  end
+
+  it "fails when we unwrap without wrapping a present first" do
+    present = Present.new
+    expect { present.unwrap }.to raise_error "No contents have been wrapped."
+  end
+
+  it "fails when a present has already been wrapped" do
+    present = Present.new
+    present.wrap("shoes")
+    expect { present.wrap("books") }.to raise_error "A contents has already been wrapped."
+  end
+
+end
+
+# context "when no task is set" do
+#   it "fails" do
+#     reminder = Reminder.new("Kay")
+#     expect { reminder.remind() }.to raise_error "No reminder set!"
+#   end
+# end
